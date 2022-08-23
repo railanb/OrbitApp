a77d0975a61491d0d1de3b7a60ddeeb7bd590496
Railan Brandão <railanb.rb@hotmail.com>
2022-08-22 21:37:25 -0300

    Página inicial concluida

--- includes/newsletter.php
+++ includes/newsletter.php
@@ -4,12 +4,12 @@
             Newsletter
         </p>
         <p class="news-p2">
-            Cadastre seu e-mail em nossa newsletter e receba gratuitamente conteúdos 
+            Cadastre seu e-mail em nossa newsletter e receba gratuitamente conteúdos
             relevantes, promoções e novidades da Orbit App
         </p>
     </div>
-    <div class="news-form">
-        <form action="">
+    <div class="">
+        <form class="news-form" action="">
             <input type="email" placeholder="Seu melhor e-mail" focus="off">
             <a href="#">
                 <button class="btn-1 btn-news" type="submit">Cadastrar</button>
--- index.php
+++ index.php
@@ -32,16 +32,72 @@
                     ganha autonomia, rapidez <br>
                     e muito dinheiro
                 </p>
-                <p class="text3-video"> 
-                Lorem ipsum dolor sit amet, consectetur <br>
-                adipiscing elit, sed do eiusmod tempor incididunt <br>
-                ut labore et dolore magna aliqua.
+                <p class="text3-video">
+                    Lorem ipsum dolor sit amet, consectetur <br>
+                    adipiscing elit, sed do eiusmod tempor incididunt <br>
+                    ut labore et dolore magna aliqua.
                 </p>
             </div>
         </div>
 
     </div>
-    
+    <div class="experiencia">
+        <div class="container experiencia">
+            <div>
+                <p class="text2-video">
+                    A melhor experiência <br>
+                    para a sua empresa
+                </p>
+                <p class="text3-video">
+                    Lorem ipsum dolor sit amet, consectetur <br>
+                    adipiscing elit, sed do eiusmod tempor <br>
+                    incididunt ut labore et.
+                </p>
+                <div class="exp">
+                    <div class="exp-itens">
+                        <img src="./imgs/icon-check.png" alt="Imagem ilustrativa">
+                        <p class="text3-video">
+                            Lorem ipsum dolor sit amet
+                        </p>
+                    </div>
+                    <div class="exp-itens">
+                        <img src="./imgs/icon-check.png" alt="Imagem ilustrativa">
+                        <p class="text3-video">
+                            Consectetur adipiscing
+                        </p>
+                    </div>
+                    <div class="exp-itens">
+                        <img src="./imgs/icon-check.png" alt="Imagem ilustrativa">
+                        <p class="text3-video">
+                            Elit, sed do eiusmod
+                        </p>
+                    </div>
+                    <button class=" btn-1 exp-btn"> Fazer Download </button>
+                </div>
+            </div>
+            <div class="integracao">
+                <img class="inte-img" src="./imgs/expriencia.png" alt="Imagem ilustrativa">
+                <div class="inte-infor">
+                    <img src="./imgs/icon-config.png" alt="Imagem icon">
+                    <div>
+                        <p class="text-p1">
+                            Integração
+                        </p>
+                        <div>
+                            <p class="text3-video">
+                                Lorem ipsum dolor sit <br>
+                                amet, consectetur
+                                adipiscing elit,<br>
+                                sed do eiusmod tempor.
+                            </p>
+                            <button class=" btn-1 banner-btn"> Saiba Mais </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <?php include './includes/newsletter.php'; ?>
     <?php include './includes/footer.php'; ?>
